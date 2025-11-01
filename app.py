32f20c61aa86dc0571488d01489ac833cdbf1461
Anshuman Thakur <anshumant711@gmail.com>
2025-11-01 23:31:26 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import json
 from sentence_transformers import SentenceTransformer
-from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
